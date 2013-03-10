ddcadb52d1e4354d7b1e0e24348c9a3dc72c6b5f
stefan <s.sobek@gmail.com>
2013-03-10 14:22:33 +0100

    Add abstract class which are extended by all generated schema classes

---
+++ src/main/java/de/feu/plib/xml/AbstractXMLType.java
@@ -0,0 +1,9 @@
+package de.feu.plib.xml;
+
+/**
+ * This is the super class for all generated Schema classes by JAXB xjc. 
+ * Provide general behavior here.  
+ */
+public abstract class AbstractXMLType {
+
+}
