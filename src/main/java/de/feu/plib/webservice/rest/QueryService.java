a95f4dc15fe9468d796cba0fa36de4448ece51f0
stefan <s.sobek@gmail.com>
2013-03-20 16:21:00 +0100

    New generated sources of xsd. Now without subclassing.

--- src/main/java/de/feu/plib/webservice/rest/QueryService.java
+++ src/main/java/de/feu/plib/webservice/rest/QueryService.java
@@ -1,6 +1,6 @@
 package de.feu.plib.webservice.rest;
 
-import java.io.ByteArrayInputStream;
+import java.io.StringReader;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -8,11 +8,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.log4j.Logger;
 
+import de.feu.plib.xml.catalogue.ItemType;
 import de.feu.plib.xml.query.QueryType;
 
 /**
@@ -27,40 +33,75 @@ import de.feu.plib.xml.query.QueryType;
 @Path("/ws")
 public class QueryService {
 
-	/** Logger instance */
-	private static final Logger LOGGER = Logger.getLogger(QueryService.class);
+    /** Logger instance */
+    private static final Logger LOGGER = Logger.getLogger(QueryService.class);
 
-	/**
-	 * Entry point which takes the xml string and after that starts the
-	 * processing of the xml. The XML must be according to ISO/TS 29002-31 -
-	 * query.xsd schema. The result is an XML file according to ISO/TS 29002-10
-	 * - catalogue.xsd where item is holding the items
-	 * 
-	 * @param queryXML
-	 *            contains the query xml file
-	 * @return The XML file holding the result of the query. Holding items.
-	 */
-	@POST
-	@Path("/query")
-	@Consumes("application/xml")
-	@Produces(MediaType.APPLICATION_XML)
-	public String query(String queryXML) {
-		LOGGER.info("Incoming query XML content :" + queryXML);
-		unmarshallXML(queryXML);
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey </hello>";
-	}
+    /**
+     * Entry point which takes the xml string and after that starts the
+     * processing of the xml. The XML must be according to ISO/TS 29002-31 -
+     * query.xsd schema. The result is an XML file according to ISO/TS 29002-10
+     * - catalogue.xsd where item is holding the items
+     * 
+     * @param queryXML
+     *            contains the query xml file
+     * @return The XML file holding the result of the query. Holding items.
+     */
+    @POST
+    @Path("/query")
+    @Consumes("application/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public String query(String queryXML) {
+        LOGGER.info("Incoming query XML content :" + queryXML);
+        QueryType queryType = unmarshallXML(queryXML);
+        marshall();
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey </hello>";
+    }
 
-	private void unmarshallXML(String xmlString) {
-		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(QueryType.class);
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			ByteArrayInputStream bs = new ByteArrayInputStream(xmlString.getBytes());
-			QueryType q = (QueryType) jaxbUnmarshaller.unmarshal(bs);
-			LOGGER.info(q);
-		} catch (JAXBException e) {
-			throw new RuntimeException("Error during unmarshalling of XML.",
-					e.getCause());
+    /**
+     * Unmarshalls the given xmlString. Note that we use {@link StreamSource}
+     * with a {@link StringReader} and then unmarshalling that with jaxb. The
+     * big benefit is that we do not need an @XMLRootElement annotation on the
+     * xml file which will normally not be generated. Great is that we do not
+     * need casting.
+     * 
+     * @param xmlString
+     *            the given string which holds the xml
+     */
+    private QueryType unmarshallXML(String xmlString) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(QueryType.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Source xmlSource = new StreamSource(new java.io.StringReader(xmlString));
+            JAXBElement<QueryType> queryElement = jaxbUnmarshaller.unmarshal(xmlSource, QueryType.class);
+            QueryType query = queryElement.getValue();
+            LOGGER.info(query);
+            return query;
+        } catch (JAXBException e) {
+            throw new RuntimeException("Error during unmarshalling of XML.", e.getCause());
+        }
+    }
 
-		}
-	}
+    private void marshall() {
+
+        ItemType item = new ItemType();
+        item.setClassRef("abc");
+
+        // create JAXB context and instantiate marshaller
+        JAXBContext context;
+        try {
+            context = JAXBContext.newInstance(ItemType.class);
+
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+           // JAXBElement<ItemType> jaxbElement = new JAXBElement<ItemType>(new QName(
+             //       "http://www.namespace.com/schema/My", "My"), ItemType.class, item);
+
+            m.marshal(item, System.out);
+
+        } catch (JAXBException e) {
+            // TODO Check if logging exception is enough!
+            LOGGER.error(e);
+        }
+    }
 }
